7d50979e5b7c9f8b21021e2a42a25714efa04952
Jinha <wlsgk5535@gmail.com>
2024-10-18 01:17:18 +0900

    ++실제 앱을 수행했을 때 갤러리로 넘어가지 않는 문제가 발생하여 GalleryActivity.java 파일에 READ_MEDIA_IMAGES 권한을 추가(안드로이드 13이상 부터 적용) ++향수 추천 시스템 추가

--- app/src/main/java/com/example/calculator/GalleryActivity.java
+++ app/src/main/java/com/example/calculator/GalleryActivity.java
@@ -6,44 +6,28 @@ import android.widget.Button;
 import android.provider.MediaStore;
 import android.content.Intent;
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.widget.Button;
-import android.widget.ImageView;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-
-import android.os.Bundle;
-
-import com.google.firebase.Firebase;
+import android.widget.Toast;
+import android.util.Log;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import org.tensorflow.lite.Interpreter;
 import java.io.File;
 import java.io.IOException;
-import com.google.firebase.FirebaseApp;
-
-// Firebase Storage 관련 import
-
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import android.util.Log;
-
-// 필요한 라이브러리 임포트
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import org.tensorflow.lite.Interpreter;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 
 public class GalleryActivity extends AppCompatActivity {
 
@@ -129,21 +113,26 @@ public class GalleryActivity extends AppCompatActivity {
     private void openGallery() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         galleryLauncher.launch(galleryIntent);
-        //downloadModelFile();
     }
 
-    // Check Permissions
+    // Check Permissions (API 레벨 33 이상에서는 READ_MEDIA_IMAGES 권한을 확인)
     private boolean checkPermissions() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        }
     }
 
     // Request Permissions
     private void requestPermissions() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                PERMISSIONS_REQUEST_CODE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CODE);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CODE);
+        }
     }
 
     // Handle the permission request result
@@ -154,7 +143,7 @@ public class GalleryActivity extends AppCompatActivity {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted
             } else {
-                // Permission denied, handle accordingly
+                Toast.makeText(this, "권한이 필요합니다.", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -172,7 +161,6 @@ public class GalleryActivity extends AppCompatActivity {
             // 파일 다운로드
             storageRef.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
                 Log.d("Firebase", "Download successful: " + localFile.getAbsolutePath());
-                // 모델 다운로드 후 이미지 예측 수행 (예시로 imageUri 사용)
                 if (imageUri != null) {
                     loadModelAndRunInference(localFile, imageUri); // 다운로드 후 이미지 예측 수행
                 }
@@ -181,7 +169,6 @@ public class GalleryActivity extends AppCompatActivity {
                 Log.d("Firebase", "Download failed", exception);
             });
         } catch (IOException e) {
-            // 예외 처리
             e.printStackTrace();
         }
     }
@@ -198,7 +185,6 @@ public class GalleryActivity extends AppCompatActivity {
             float[][] output = new float[1][4]; // Output array size (adjust based on your model output size)
             tflite.run(inputBuffer, output);
 
-            // 결과 처리
             String[] labels = {"businesscasual", "casual", "dandy", "street"}; // 실제 라벨에 맞게 수정
             int maxIndex = getMaxIndex(output[0]);
             Log.d("TensorFlowLite", "Predicted label: " + labels[maxIndex] + " (" + output[0][maxIndex] * 100 + "%)");
@@ -214,15 +200,12 @@ public class GalleryActivity extends AppCompatActivity {
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * 224 * 224 * 3); // 이미지 크기에 맞게 조정
         byteBuffer.order(ByteOrder.nativeOrder());
 
-        // 이미지 크기 조정
         Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
         int[] intValues = new int[224 * 224];
         scaledBitmap.getPixels(intValues, 0, scaledBitmap.getWidth(), 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight());
 
-        // ImageNet의 평균값: BGR 순서로 설정
         float[] mean = {103.939f, 116.779f, 123.68f};
 
-        // BGR 순서로 각 픽셀 값에서 평균값을 빼고 ByteBuffer에 추가
         for (int pixel : intValues) {
             int b = (pixel) & 0xFF;
             int g = (pixel >> 8) & 0xFF;
@@ -236,8 +219,6 @@ public class GalleryActivity extends AppCompatActivity {
         return byteBuffer;
     }
 
-
-
     // 결과 배열에서 최댓값의 인덱스를 구하는 메서드
     private int getMaxIndex(float[] arr) {
         int index = 0;
@@ -250,11 +231,4 @@ public class GalleryActivity extends AppCompatActivity {
         }
         return index;
     }
-
-
-
 }
-
-
-
-
